18ead146fbcbbfa23c57a562c46cccea551b7eb7
taunioned <taunioned@msn.cn>
2016-01-04 23:15:14 +0800

    v20160104-2315

--- app/src/main/java/com/tau/blwatch/TimeHelper.java
+++ app/src/main/java/com/tau/blwatch/TimeHelper.java
@@ -6,6 +6,10 @@ import java.util.Calendar;
  * Created by John on 2015/12/30.
  */
 public class TimeHelper {
+
+    public final static int MONTH_OF_YEAR = 12;    //1年=12个月
+    public final static int WEEK_PER_QUARTER = 13; //1季度=13个星期
+
     public static int DAY_PER_MONTH = 31;
     public static int DAY_PER_QUARTER = 90;
     static {
@@ -20,14 +24,11 @@ public class TimeHelper {
         DAY_PER_QUARTER += mCalendar.get(Calendar.DATE);    //1季度=90/91天
     }
 
-    public final static int POINT_PER_LINE_OF_YEAR = 12;    //1年=12个月
-    public final static int POINT_PER_LINE_OF_QUARTER = 13; //1季度=13个星期
-    public final static int POINT_PER_LINE_OF_MONTH = DAY_PER_MONTH;
-    public final static int POINT_PER_LINE_OF_WEEK = 7;     //1星期=7天
-    public final static int POINT_PER_LINE_OF_DAY = 24;      //1天=24小时
+    public final static int DAY_PER_WEEK = 7;     //1星期=7天
+    public final static int HOUR_PER_DAY = 24;      //1天=24小时
 
-    public final static long MI_SECOND_YEAR = (long)365 * 24 * 3600 * 1000;
-    public final static long MI_SECOND_QUARTER = (long)365 * 24 * 3600 * 1000;
+//    public final static long MI_SECOND_YEAR = (long)365 * 24 * 3600 * 1000;
+//    public final static long MI_SECOND_QUARTER = (long)365 * 24 * 3600 * 1000;
     public final static long MI_SECOND_MONTH = (long)DAY_PER_MONTH * 24 * 3600 * 1000;
     public final static long MI_SECOND_WEEK = (long)7 * 24 * 3600 * 1000;
     public final static long MI_SECOND_DAY = (long)24 * 3600 * 1000;
