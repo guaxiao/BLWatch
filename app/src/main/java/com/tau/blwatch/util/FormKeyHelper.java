fbadd3e74e535ab79d0b0fb949ac72e8c4dc01c3
TAUnionEd <taunioned@msn.cn>
2016-01-17 14:24:31 +0800

    新增工具类，用于统一的记录http表单中的key

---
+++ app/src/main/java/com/tau/blwatch/util/FormKeyHelper.java
@@ -0,0 +1,15 @@
+package com.tau.blwatch.util;
+
+public class FormKeyHelper {
+    public static final String action = "Action";
+
+    public static final String user_id = "user_ID";
+    public static final String user_name = "user_name";
+    public static final String user_password = "password";
+    public static final String user_imageUrl = "imageUrl";
+    public static final String is_login = "isLogin";
+    public static final String bluetooth_address = "Bluetooth";
+    public static final String upload_user_id = "userid";
+    public static final String upload_step = "Step";
+    public static final String upload_heart = "Heart";
+}
