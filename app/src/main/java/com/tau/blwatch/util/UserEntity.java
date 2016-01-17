ddf5ba7e964571d2e1fbd5ecd928b3a3e32e71d4
TAUnionEd <taunioned@msn.cn>
2016-01-17 14:44:10 +0800

    新增用户信息工具类，实现Parcelable接口以做传输 存储用户ID、用户名和头像地址

---
+++ app/src/main/java/com/tau/blwatch/util/UserEntity.java
@@ -0,0 +1,65 @@
+package com.tau.blwatch.util;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class UserEntity implements Parcelable {
+
+    private String userID;
+    private String userName;
+    private String imgPath;
+
+    public UserEntity(Parcel source){
+        userID = source.readString();
+        userName = source.readString();
+        imgPath = source.readString();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserImgPath() {
+        return imgPath;
+    }
+
+    public void setUserImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(userID);
+        dest.writeString(userName);
+        dest.writeString(imgPath);
+    }
+
+    public static final Parcelable.Creator<?> CREATOR = new Parcelable.Creator() {
+        @Override
+        public UserEntity createFromParcel(Parcel source) {
+            return new UserEntity(source);
+        }
+
+        @Override
+        public UserEntity[] newArray(int size) {
+            return new UserEntity[size];
+        }
+    };
+}
