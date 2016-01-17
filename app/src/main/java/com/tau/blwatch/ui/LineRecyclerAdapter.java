5c8bdaff40b757944e69cd6eeaf2ccf64ecfef7c
TAUnionEd <taunioned@msn.cn>
2016-01-17 14:45:57 +0800

    转移至ui包

--- app/src/main/java/com/tau/blwatch/ui/LineRecyclerAdapter.java
+++ app/src/main/java/com/tau/blwatch/ui/LineRecyclerAdapter.java
@@ -1,4 +1,4 @@
-package com.tau.blwatch;
+package com.tau.blwatch.ui;
 
 import android.content.Context;
 import android.graphics.Color;
@@ -8,8 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import javax.crypto.spec.PSource;
 
 /**
  * Created by Administrator on 2015/12/19 0019.
