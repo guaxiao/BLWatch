ea5b8a4a4a78f4bc7c903f80042a240a5ef702d6
taunioned <taunioned@msn.cn>
2016-04-12 10:56:47 +0800

    新增一个查询设备类型MAC地址范围的方法

--- app/src/main/java/com/tau/blwatch/callBack/DataBaseTranslator.java
+++ app/src/main/java/com/tau/blwatch/callBack/DataBaseTranslator.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public interface DataBaseTranslator {
-    HashMap<String,ArrayList<Float>> onSelectData(long startTime, int numBlock, String typeTimeBlock, String tableName);
+    HashMap<String,ArrayList<Float>> onSelectChartData(long startTime, int numBlock, String typeTimeBlock, String tableName);
+    ArrayList<String> onSelectMacPair(String deviceType, String tableName);
     void onSendHeartToDB(int avgHeart, int maxHeart, int minHeart);
     void onSendStepToDB(int countStep);
     void onSendWeightToDB(double countWeight);
